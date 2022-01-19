3ab5edbf35c350a777cb3de9b2fe3a21b160eef3
Cade Jacobson <91996442+cadejacobson@users.noreply.github.com>
2022-01-18 20:06:48 -0700

    palindrome.py - added comments for readability

--- palindrome.py
+++ palindrome.py
@@ -7,13 +7,13 @@ x = 0
 
 def palindrome( string ):
     for x in length:
-        L = 0 - (x + 1)
-        pos = string[L]
-        invertedList.append(pos)
+        L = 0 - (x + 1)         #access the negative index of the next position
+        pos = string[L]         #store the next index into the position variable
+        invertedList.append(pos)    #append that value onto the end of the 
     return invertedList
 
 
 palindrome(string)
 
-finalPalindrome = "".join(invertedList)
+finalPalindrome = "".join(invertedList)   #convert an empty string and a list into a  single string
 print(finalPalindrome)
