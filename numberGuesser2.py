5bd3c47fca7662005f4a423f55b6123a8617598e
Cade Jacobson <91996442+cadejacobson@users.noreply.github.com>
2022-01-18 20:06:20 -0700

    NumberGuesser2.py - added comments for readability

--- numberGuesser2.py
+++ numberGuesser2.py
@@ -3,23 +3,22 @@ print("Please enter integers only.")
 print("Please enter the low end of the range:")
 lowRange = int(input())
 
-
 print("Please enter the high end of the range:")
 highRange = int(input())
 
 
-midpoint = round((highRange + lowRange) / 2)
+midpoint = round((highRange + lowRange) / 2)      #calculate the midpoint for the initial guess
 print("Is " + str(midpoint) + " your number?")
 print("Please respond \'y\', \'higher\' or \'lower\' ")
 userResponse = input()
 
-while userResponse != 'y':
+while userResponse != 'y':                       #loop until the user types a confirmation
     if userResponse == "higher":
         lowRange = midpoint
     if userResponse == "lower":
-        highRange = midpoint
+        highRange = midpoint                    
     midpoint = round((highRange + lowRange) / 2)
-    print("Is " + str(midpoint) + " your number?")
+    print("Is " + str(midpoint) + " your number?")    #ask the user if the guess is correct
     print("Please respond \'y\', \'higher\' or \'lower\' ")
     userResponse = input()
 
