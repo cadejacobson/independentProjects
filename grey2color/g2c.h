64a49d67a76aaf02005e368707835c8307a8a775
Cade Jacobson <91996442+cadejacobson@users.noreply.github.com>
2022-04-14 10:23:43 -0600

    completed memory allocation

--- grey2color/g2c.h
+++ grey2color/g2c.h
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
+typedef unsigned char pixel;
+
+#ifndef __image_struct__
+#define __image_struct__
+
+struct image
+{
+    string magicNumber;
+    string comment;
+    int rows;
+    int cols;
+    pixel** redGray;
+    pixel** green;
+    pixel** blue;
+};
+
+#endif
--- grey2color/memory.cpp
+++ grey2color/memory.cpp
@@ -1,1 +1,90 @@
 #include "g2c.h"
+
+
+
+
+
+
+
+bool allocateGray(image& picture)
+{
+    picture.redGray = nullptr;
+    int i;
+
+    picture.redGray = new (nothrow) pixel * [picture.rows];
+    if (picture.redGray == nullptr)       //allocate three pointers of pointers
+    {
+        cout << "Unable to allocate memory." << endl;
+        return false;
+    }
+
+    for (i = 0; i < picture.rows; i += 1)   //allocate an array for each 
+    {                                       //row with the size of the columns.
+        picture.redGray[i] = new (nothrow) pixel[picture.cols];
+        if (picture.redGray[i] == nullptr)
+        {
+            cout << "Unable to allocate memory." << endl;
+            return false;
+        }
+
+    }
+
+    return true;
+}
+
+
+
+bool allocateColor(image& picture)
+{
+    picture.redGray = nullptr;
+    picture.blue = nullptr;
+    picture.green = nullptr;
+    int i;
+
+    picture.redGray = new (nothrow) pixel * [picture.rows];
+    if (picture.redGray == nullptr)       //allocate three pointers of pointers
+    {
+        cout << "Unable to allocate memory." << endl;
+        return false;
+    }
+
+    picture.blue = new (nothrow) pixel * [picture.rows];
+    if (picture.blue == nullptr)
+    {
+        cout << "Unable to allocate memory." << endl;
+        return false;
+    }
+
+    picture.green = new (nothrow) pixel * [picture.rows];
+    if (picture.green == nullptr)
+    {
+        cout << "Unable to allocate memory." << endl;
+        return false;
+    }
+
+    for (i = 0; i < picture.rows; i += 1)   //allocate an array for each 
+    {                                       //row with the size of the columns.
+        picture.redGray[i] = new (nothrow) pixel[picture.cols];
+        if (picture.redGray[i] == nullptr)
+        {
+            cout << "Unable to allocate memory." << endl;
+            return false;
+        }
+
+        picture.blue[i] = new (nothrow) pixel[picture.cols];
+        if (picture.blue[i] == nullptr)
+        {
+            cout << "Unable to allocate memory." << endl;
+            return false;
+        }
+
+        picture.green[i] = new (nothrow) pixel[picture.cols];
+        if (picture.green[i] == nullptr)
+        {
+            cout << "Unable to allocate memory." << endl;
+            return false;
+        }
+    }
+
+    return true;
+}
